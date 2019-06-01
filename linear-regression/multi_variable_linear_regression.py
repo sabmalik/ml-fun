fc4ccfb146b7581b3c3ddafe482e365a96b868a0
sab <smalik@ea.com>
2019-06-01 23:48:32 +1000

    simplify the example:

--- linear-regression/multi_variable_linear_regression.py
+++ linear-regression/multi_variable_linear_regression.py
@@ -1,7 +1,6 @@
 import pandas as pd  
 import numpy as np  
 import matplotlib.pyplot as plt  
-import seaborn as sns
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
@@ -16,30 +15,36 @@ print(dataset.shape)
 print(dataset.describe())
 
 # we always call the independent variables as X
-X = dataset[['Bed Rooms', 'Bath Rooms', 'Land size sqm']]
+X = dataset[['Bedrooms', 'Bathrooms', 'Land size sqm']]
 
 # the dependent variable is called y
 y = dataset['Price']
 
-# show the distribution of prices
-plt.figure(figsize=(8,4))
-plt.tight_layout()
-sns.distplot(y)
-
+# lets split our data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+# use regression on our training data
 regressor = LinearRegression()  
 regressor.fit(X_train, y_train)
 
+# get and show us the coefficients/weights
 coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])  
 print(coeff_df)
 
+# do some predictions on the test dataset
 y_pred = regressor.predict(X_test)
 
+# show us the differences in actual and predicted prices
 df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
 df1 = df.head(25)
 
+# plot them for us please
 df1.plot(kind='bar',figsize=(8,4))
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
+
+# show some fancy metrics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
