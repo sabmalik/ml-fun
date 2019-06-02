a655b620c3a1f345ea31d151ac080c92192d5121
sab <smalik@ea.com>
2019-06-02 23:21:43 +1000

    confusion matrix

--- logistic-regression/logistic_regression.py
+++ logistic-regression/logistic_regression.py
@@ -1,8 +1,12 @@
 # load up the libraries we will need
 import pandas as pd  
+import numpy as np  
+import seaborn as sns
+import matplotlib.pyplot as plt  
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+from sklearn import metrics
 
 # load the dataset from the csv file
 dataset = pd.read_csv('house-prices.csv')
@@ -41,3 +45,22 @@ y_pred = regressor.predict(X_test)
 
 # print the accuracy report
 print(classification_report(y_test,y_pred))
+
+# import the metrics class
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+
+class_names = [0,1] # name  of classes
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+# create heatmap
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+plt.show()
